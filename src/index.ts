3fddf8fb85811369213917552a42ef1509bf0aec
Karina Barinova <kbarinova11@gmail.com>
2022-12-19 13:53:18 +0000

    Test local type overrides for react

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import App from './ui/App';
 
-function initializeReactApp() {
-  const appContainer = document.getElementById('appContainer');
-  if (!appContainer) throw new Error('No #appContainer found in DOM');
-  render(React.createElement(App), appContainer);
+import { foo } from 'react'
+
+function initializeReactApp ()
+{
+  const appContainer = document.getElementById( 'appContainer' );
+  if ( !appContainer ) throw new Error( 'No #appContainer found in DOM' );
+  render( React.createElement( App ), appContainer );
 }
 
 initializeReactApp();
